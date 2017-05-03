5bec3ae2c3c298fb1fb66b0069cb8c6ea6c5b89e
Reed Kinning <reed@flowroute.com>
2017-05-03 13:53:03 -0700

    Default to running service locally with env variables, docker-compose sets up its own environment

--- appointment_reminder/settings.py
+++ appointment_reminder/settings.py
@@ -2,13 +2,17 @@
 from __future__ import unicode_literals
 
 import os
+from environs import Env
 
-FLOWROUTE_SECRET_KEY = os.environ['FLOWROUTE_SECRET_KEY']
-FLOWROUTE_ACCESS_KEY = os.environ['FLOWROUTE_ACCESS_KEY']
-FLOWROUTE_NUMBER = os.environ['FLOWROUTE_NUMBER']
+env = Env()
+env.read_env()
+
+FLOWROUTE_SECRET_KEY = env('FLOWROUTE_SECRET_KEY')
+FLOWROUTE_ACCESS_KEY = env('FLOWROUTE_ACCESS_KEY')
+FLOWROUTE_NUMBER = env('FLOWROUTE_NUMBER')
 
 # Turn this to False when in production
-is_debug = os.environ.get('DEBUG_MODE', 'True')
+is_debug = env('DEBUG_MODE', 'True')
 if is_debug.lower() == 'true':
     DEBUG_MODE = True
 else:
@@ -17,18 +21,18 @@ else:
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-TEST_DB = "sqlite:////var/lib/sqlite/data/test_appt_reminder.db"
-DB = "sqlite:////var/lib/sqlite/data/appt_reminder.db"
+TEST_DB = "sqlite:///sqlite/test_appt_reminder.db"
+DB = "sqlite:///sqlite/appt_reminder.db"
 
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL',
-                                   'redis://redis:6379')
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND',
-                                       'redis://redis:6379')
+CELERY_BROKER_URL = env('CELERY_BROKER_URL',
+                        'redis://localhost:6379')
+CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND',
+                            'redis://localhost:6379')
 CELERY_ENABLE_UTC = True
 
 # see arrow.locales for supported languages
 LANGUAGE_DEFAULT = 'en_us'
-ORG_NAME = os.environ.get('ORG_NAME', 'Your Org Name')
+ORG_NAME = env('ORG_NAME')
 
 MSG_TEMPLATE = (u"[{}]\nYou have an appointment on {{}}{{}}. "
                 u"Please reply 'Yes' to confirm, or 'No' "
